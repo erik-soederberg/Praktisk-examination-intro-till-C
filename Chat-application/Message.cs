3c3baf7b83aa579956e2f2c3a090a19c1fd66fd2
eriksoderb3rg <eriksoederberg@outlook.com>
2025-11-11 09:48:32 +0100

    Add: Base message class + constructor

--- Chat-application/Message.cs
+++ Chat-application/Message.cs
@@ -2,5 +2,17 @@ namespace Praktisk_examination_intro_till_C;
 
 public class Message
 {
+    public DateTime TimeStamp { get; }
+    public string User { get; }
+    public string Messages { get; }
+
+    public Message(DateTime timestamp, string user, string messages)
+    {
+        User = user;
+        Messages = messages;
+        TimeStamp = timestamp;
+    }
     
 }
+
+
